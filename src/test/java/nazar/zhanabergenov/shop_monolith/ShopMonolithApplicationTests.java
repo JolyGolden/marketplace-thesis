9136a322e407825c472e421390707f13d74d8e05
Nazar <kateryna@Katerynas-MacBook-Pro.local>
2025-07-08 20:29:33 +0200

    first commit

---
+++ src/test/java/nazar/zhanabergenov/shop_monolith/ShopMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package nazar.zhanabergenov.shop_monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
